e20e39e7cd40b013cb926b4826c9f8dac33c079f
AndresMorquecho <morquechoisrael@hotmail.com>
2025-10-20 15:18:49 -0500

    Reto 6

--- modulo9/proyecto1/aprender-angular/src/app/components/mi-perfil/mi-perfil.ts
+++ modulo9/proyecto1/aprender-angular/src/app/components/mi-perfil/mi-perfil.ts
@@ -9,9 +9,20 @@ import { Component } from '@angular/core';
 
 export class MiPerfil {
 
+  public name: string;
+  public age: number;
+  public address: string;
+  public phone: string;
+  public profession: string;
+
+
   constructor() {
+    this.name = "Andres Israel";
+    this.age = 26.0;
+    this.address = "Calle Falsa 123";
+    this.phone = "120938109238"
+    this.profession = "Desarrollador de Software";
     console.log("Se cargo el componente de perfil");
-    alert("Bienvenido a mi perfil");
   }
 
 
