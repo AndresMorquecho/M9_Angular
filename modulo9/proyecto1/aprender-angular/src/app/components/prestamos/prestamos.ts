eec1a1c53dcc026097c11010e72539412342562f
AndresMorquecho <morquechoisrael@hotmail.com>
2025-10-22 22:30:52 -0500

    Reto

--- modulo9/proyecto1/aprender-angular/src/app/components/prestamos/prestamos.ts
+++ modulo9/proyecto1/aprender-angular/src/app/components/prestamos/prestamos.ts
@@ -10,13 +10,21 @@ export class Prestamos {
 
   public web = "https://open.spotify.com/intl-es/";
   public redirigir = true;
+  public prestamosList = [
+    { id:1, monto: 200, estado: 'Aprobado'},
+    { id:2, monto: 400, estado: 'Aprobado'},
+    { id:3, monto: 200, estado: 'Pendiente'},
+    { id:4, monto: 100, estado: 'Pendiente'},
+    { id:5, monto: 700, estado: 'Pendiente'},
+    { id:6, monto: 800, estado: 'Aprobado'},
+  ];
 
 
-  cambiarEstado(){
-    if(this.redirigir){
+  cambiarEstado() {
+    if (this.redirigir) {
       this.redirigir = false;
       this.web = "";
-    }else{
+    } else {
       this.redirigir = true;
       this.web = "https://open.spotify.com/intl-es/";
     }
