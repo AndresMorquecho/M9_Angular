9f2c7711b56c3a554e0ff4a8173be6c5ca5c0a27
AndresMorquecho <morquechoisrael@hotmail.com>
2025-11-24 21:20:49 -0500

    Reto 22 Páginas y Rutas

--- modulo9/proyecto1/aprender-angular/src/app/app.routes.ts
+++ modulo9/proyecto1/aprender-angular/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MiPerfil } from './components/mi-perfil/mi-perfil';
+import { transacciones } from './models/transaccion';
+import { Cuenta } from './components/cuenta/cuenta';
+import { Cine } from './components/cine/cine';
+import { Pagina404 } from './components/pagina404/pagina404';
+import { Transacciones } from './components/transacciones/transacciones';
+import { Prestamos } from './components/prestamos/prestamos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: MiPerfil,  },
+    {path: "inicio", component: MiPerfil,  },
+    {path: "transaccion", component:Transacciones ,  },
+    {path: "cuenta", component: Cuenta ,  },
+    {path: "prestamos", component:Prestamos ,  },
+    {path: "**", component:Pagina404 },
+];
--- modulo9/proyecto1/aprender-angular/src/app/app.ts
+++ modulo9/proyecto1/aprender-angular/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { config } from './models/config';
 
 import { MiPerfil } from "./components/mi-perfil/mi-perfil";
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [MiPerfil],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
